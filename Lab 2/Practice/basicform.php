af8f9c799cb5328dcee64c682d2644944d54f249
Phien <tranngocphien0309@gmail.com>
2021-03-31 23:16:40 +0700

    Pratice Lab2_1

---
+++ Lab 2/Practice/basicform.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title> A Simple Form</title>
+    <body>
+        <form action="" method="POST">
+            Click submit to start our initial PHP program.
+            <br>
+            <input type ="text" value="Name">
+            <br>
+            <input type="submit" value="Click To Submit">
+            <input type="reset" value="Earse and Restart">
+            
+        </form>
+    </body>
+    </head>
+</html>
---
+++ Lab 2/Practice/registrationform.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title> A Simple Form</title>
+    <body>
+        <form action="" method="POST">
+            Click submit to start our initial PHP program.
+            <br>
+            <br>
+            <input type ="text" value="Name">
+            <br>
+            <br>
+            <input type="password" value="Password">
+            <br>
+            <br>
+            <input type="submit" value="Click To Submit">
+            <input type="reset" value="Earse and Restart">
+            
+        </form>
+    </body>
+    </head>
+</html>
