1db5bd6cf1ab0f369992ba4f39a819414369fa22
Phien <tranngocphien0309@gmail.com>
2021-04-01 19:42:46 +0700

    Update Lab 2-1

--- Lab 2/Exercise/Lab2_1_handle.php
+++ Lab 2/Exercise/Lab2_1_handle.php
@@ -1,19 +1,20 @@
 <html>
     <head>
+        <meta charset="utf-8"/>
         <title> Receiving Input</title>
     </head>
     <body>
         <?php
             $name = $_POST["name"];
-			$birth= $_POST["birth"];
             $university = $_POST["university"];
             $class = $_POST["class"];
             $hobby = $_POST["hobby"];
+            $birth = $_POST["Birth"];
             print ("Hello, $name ");
-            print ("<br>");	
-			print ("Your birthday:, $birth");
-			print ("<br>");
+            print ("<br>");
             print ("You are studing at $class, $university");
+            print ("<br>");
+            print ("Your birthday: $birth");
             print ("<br>");
             print ("Your hobby is: ");
             $i = 1;
