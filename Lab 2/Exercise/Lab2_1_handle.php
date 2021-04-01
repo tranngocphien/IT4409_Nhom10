93865c3bba38df28c03b4ba6da4841eb59692f09
Phien <tranngocphien0309@gmail.com>
2021-04-01 16:34:36 +0700

    Update exercises lab 2.1

---
+++ Lab 2/Exercise/Lab2_1_handle.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title> Receiving Input</title>
+    </head>
+    <body>
+        <?php
+            $name = $_POST["name"];
+            $university = $_POST["university"];
+            $class = $_POST["class"];
+            $hobby = $_POST["hobby"];
+            print ("Hello, $name ");
+            print ("<br>");
+            print ("You are studing at $class, $university");
+            print ("<br>");
+            print ("Your hobby is: ");
+            $i = 1;
+            foreach ($hobby as $selected) {
+                print ("<ul>$i, $selected</ul>");
+                $i++;
+            }
+            
+        ?>
+    </body>
+</html>
