ab45c2e73a76a5abbeefd5c76b6a4d3ff05ad4d8
Tran Ngoc Phien <tranngocphien0309@gmail.com>
2021-04-29 22:34:40 +0700

    Lab 4

---
+++ lab4/E-commerce/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/** Configuration Variables **/
+
+define ('DEVELOPMENT_ENVIRONMENT',true);
+
+
+define('DB_NAME', 'todo');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+
+define('BASE_PATH','http://localhost/Lab4_2');
+
+
+define('PAGINATE_LIMIT', '5');
